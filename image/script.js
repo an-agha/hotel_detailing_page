08a3a4991fc18c703cb912e91734439758ec7666
an-agha <anaghanisakar@gmail.com>
2021-10-02 18:28:23 +0530

    final

---
+++ image/script.js
@@ -0,0 +1,20 @@
+// function viewDis()
+// {
+//     var view_more = document.getElementById("viewMore");
+//     var view_less = document.getElementById("viewLess");
+//     var btn_more = document.getElementById("more");
+
+//   if (view_more.style.display === "none")
+//    {
+//     view_more.style.display = "inline";
+//     btn_more.innerHTML = "Less"; 
+//     view_less.style.display = "none";
+//   }
+//    else 
+//    {
+//     view_more.style.display = "none";
+//     btn_more.innerHTML = "More"; 
+//     view_less.style.display = "inline";   
+// }
+
+// }
